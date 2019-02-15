64ac81314c1ef6646551065aab19e05648da1104
Carratu Carmine <carratucarmine@sal-26.local>
2019-02-15 10:46:24 +0100

    prova merge

--- UnisaLift/AppDelegate.swift
+++ UnisaLift/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    var Prova : String = "ProvaDi  PULL"
+    var ProvaMerge : String = "ProvaDi  PULL"
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
