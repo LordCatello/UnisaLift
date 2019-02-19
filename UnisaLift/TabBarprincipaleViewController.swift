5ce81b379891d3881c4ab7a9e2596d117792fe87
Cuomo Luca <cuomoluca@sal-30.local>
2019-02-19 17:11:51 +0100

    Ho aggiunto il tasto Fatto nel Modifica Profilo

--- UnisaLift/TabBarprincipaleViewController.swift
+++ UnisaLift/TabBarprincipaleViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ModificaProfiloViewController.swift
+//  TabBarprincipaleViewController.swift
 //  UnisaLift
 //
 //  Created by Cuomo Luca on 19/02/2019.
@@ -8,36 +8,27 @@
 
 import UIKit
 
-class ModificaProfiloViewController: UIViewController {
+class TabBarprincipaleViewController: UITabBarController {
 
-    @IBAction func FattoButtonPressed(_ sender: Any) {
-        
-   /*
-        self.tabBarController?.selectedIndex = 2
-        */
-        self.navigationController?.popViewController(animated: true)
-       // self.dismiss(animated: true, completion: nil)
-        
-        //performSegue(withIdentifier: "Impostazioni", sender: nil)
-    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+ 
     
+
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-     
-        
-        
-    
-    
     }
+    */
+
 }
