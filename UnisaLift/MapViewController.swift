d88448be422fb43b249ccc4db9788dc5f65e540f
Cafaro Davide <cafarodavide@sal-23.local>
2019-02-19 15:54:52 +0100

    aggiunta la possibilità di calcolare e stampare un percorso sulla mappa - fase di debug, premendo su login si viene indirizzati sulla mappa per ragioni di test e il percorso stampato è arbitrario

--- UnisaLift/MapViewController.swift
+++ UnisaLift/MapViewController.swift
@@ -9,14 +9,17 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
-    var locationToDisplay : CLLocation!
+    var locationToDisplay: CLLocation! // only useful as CLLocation if we want to display something other than source or destination - we could use a boolean value instead such as displaySource
+    var source: CLLocation!
+    var destination: CLLocation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.mapView.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -30,6 +33,56 @@ class MapViewController: UIViewController {
         mapView.setRegion(coordinateRegion, animated: true)
     }
 
+    // helper function which requests the path from source to dest to the apple servers
+    func requestDirectionsTo(from source : CLLocation, to destination : CLLocation) {
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: source.coordinate))
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: destination.coordinate))
+        request.transportType = .automobile
+        
+        let directions = MKDirections(request: request)
+        
+        directions.calculate { (response, error) in
+            if error == nil {
+                
+                if let r = response {
+                    let route = r.routes[0]
+                    print(route.distance, "CALCOLATA TUTT APPOST")
+                    self.mapView.addOverlay(route.polyline)
+                }
+                
+//                if let routes = response?.routes {
+//                    for route in routes {
+//                        print(route.distance)
+//                        self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.aboveRoads)
+//                    }
+//                }
+                
+            } else {
+                let alert = UIAlertController(title: "ERROR", message: "Error in calculating the directions. Please try again later.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        
+    }
+    
+    // function called from the mapView as this is its delegate in order to render the directions
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            if let overlay = overlay as? MKPolyline {
+                print("SWAG?")
+                let polylineRenderer = MKPolylineRenderer(polyline: overlay)
+                polylineRenderer.strokeColor = UIColor.init(red: 218/255, green: 83/255, blue: 18/255, alpha: 1.0)
+                return polylineRenderer
+            }
+            else {
+                return MKOverlayRenderer(overlay: overlay)
+            }
+        }
+
+    
+        
+    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -39,6 +92,8 @@ class MapViewController: UIViewController {
         
 //        centerMapOnLocation(location: locationToDisplay)
         centerMapOnLocation(location: debugLocation)
+        
+        requestDirectionsTo(from: debugLocation, to: CLLocation(latitude: 40.774001, longitude: 14.793703))
     }
     
 
