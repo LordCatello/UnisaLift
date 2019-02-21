2006052209e81b22baf59e3f8c3a63979dea0b42
Carratu Carmine <carratucarmine@sal-26.local>
2019-02-21 10:20:33 +0100

    Logout

--- UnisaLift/ImpostazioniViewController.swift
+++ UnisaLift/ImpostazioniViewController.swift
@@ -11,7 +11,12 @@ import UIKit
 class ImpostazioniViewController: UIViewController {
 
   
-   
+    @IBAction func logoutButtonPressed(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.userLogged = nil
+        performSegue(withIdentifier: "logoutSegue", sender: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
