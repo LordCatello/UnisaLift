54e7bdebf3fe073baccdf8933bf00d090a89ca99
DArmino Emanuele <you@example.com>
2019-02-21 11:21:39 +0100

    Miglioramenti finali pagina SignUp e SignIn

--- UnisaLift/SignUpViewController.swift
+++ UnisaLift/SignUpViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-extension UIStackView : UITextFieldDelegate{
+extension UIView : UITextFieldDelegate{
     override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         self.endEditing(true)
@@ -34,27 +34,26 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var TableViewElementi: UITableView!
     
-    @IBOutlet weak var nameField: UITextField!
+    
     
     @IBOutlet weak var surnameField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
     
-    @IBOutlet weak var StackViewElementi: UIStackView!
+    @IBOutlet weak var ViewElementi: UIView!
+    
     
     @IBOutlet weak var emailField: UITextField!
     
     @IBOutlet weak var carModelField: UITextField!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.carModelField.delegate = StackViewElementi
-        self.emailField.delegate = StackViewElementi
-        self.nameField.delegate = StackViewElementi
-        self.surnameField.delegate = StackViewElementi
+        self.carModelField.delegate = ViewElementi
+        self.emailField.delegate = ViewElementi
+        self.nameField.delegate = ViewElementi
+        self.surnameField.delegate = ViewElementi
         
-        nameField.setBottomBorder()
-        emailField.setBottomBorder()
-        surnameField.setBottomBorder()
-        carModelField.setBottomBorder()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -63,54 +62,3 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
 
 }
 
-/*
-class SignUpViewController: UIViewController, UITextFieldDelegate {
-
-@IBAction func signUpButton(_ sender: Any) {
-    let name = nameField.text
-    let surname = surnameField.text
-    let carModel = carModelField.text
-    
-    if(name == "" || surname == "" || carModel == "") {
-        // dovrei mostrare un messaggio in cui avverto che tutti i campi devono essere compilati
-    }
-    else {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        appDelegate.userLogged = PersistenceManager.newUser(carModel: carModel!, email: email, name: name!, surname: surname!)
-        performSegue(withIdentifier: "homeFromSignUp", sender: nil)
-    }
-}
-
-
-@IBOutlet weak var TableViewElementi: UITableView!
-
-@IBOutlet weak var nameField: UITextField!
-
-@IBOutlet weak var surnameField: UITextField!
-
-@IBOutlet weak var StackViewElementi: UIStackView!
-
-@IBOutlet weak var emailField: UITextField!
-
-@IBOutlet weak var carModelField: UITextField!
-
-override func viewDidLoad() {
-    super.viewDidLoad()
-    self.carModelField.delegate = StackViewElementi
-    self.emailField.delegate = StackViewElementi
-    self.nameField.delegate = StackViewElementi
-    self.surnameField.delegate = StackViewElementi
-    
-    nameField.setBottomBorder()
-    emailField.setBottomBorder()
-    surnameField.setBottomBorder()
-    carModelField.setBottomBorder()
-    // Do any additional setup after loading the view.
-}
-
-override func viewWillAppear(_ animated: Bool) {
-    emailField.text = email
-}
-
-}
-*/
