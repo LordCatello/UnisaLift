e4fb15125a5a892da0beaccecab80e977159e25b
Carratu Carmine <carratucarmine@sal-26.local>
2019-02-22 00:59:02 +0100

    Modifiche marginali

--- UnisaLift/DettagliRichiedenteViewController.swift
+++ UnisaLift/DettagliRichiedenteViewController.swift
@@ -19,17 +19,17 @@ class DettagliRichiedenteViewController: UIViewController {
     
     @IBAction func AcceptRequestButtonPressed(_ sender: Any) {
     }
+    
     @IBOutlet weak var applicantSurnameLabel: UILabel!
+    
     @IBOutlet weak var applicantNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        
-         var profileimage = UIImage(data: user.imageFullRes as! Data, scale:1.0)
+        var profileimage = UIImage(data: user.imageFullRes as! Data, scale:1.0)
         applicantImage.image = profileimage
         applicantNameLabel.text = user.name
         applicantSurnameLabel.text = user.surname
@@ -37,19 +37,8 @@ class DettagliRichiedenteViewController: UIViewController {
         self.tabBarController?.tabBar.isHidden = true
     }
 
-    
-    
     override func viewWillDisappear(_ animated: Bool) {
         self.tabBarController?.tabBar.isHidden = false
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
