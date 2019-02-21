3de3fa846b5fcfd2c5b861ba56cee9aa60a19e19
fedekiyomi <fede.kiyomi@gmail.com>
2019-02-21 10:40:42 +0100

    modifiche impostazioni

--- UnisaLift/ImpostazioniViewController.swift
+++ UnisaLift/ImpostazioniViewController.swift
@@ -11,7 +11,13 @@ import UIKit
 class ImpostazioniViewController: UIViewController {
 
   
-   
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var surname: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var car: UILabel!
+    @IBOutlet weak var descrizione: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
