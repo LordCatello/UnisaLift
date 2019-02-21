2fbda36a9d3e66a04a8378b4e0bb6abf9b988b2a
fedekiyomi <fede.kiyomi@gmail.com>
2019-02-21 14:28:37 +0100

    modifiche ad info.plist

--- UnisaLift/ModificaProfiloViewController.swift
+++ UnisaLift/ModificaProfiloViewController.swift
@@ -92,49 +92,6 @@ class ModificaProfiloViewController: UIViewController,UIImagePickerControllerDel
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func takePicture(_ sender: Any) {
-        
-       
-            let imagePicker = UIImagePickerController()
-            //challenge
-            imagePicker.setEditing(true, animated: true)
-            
-            
-            // If the device has a camera, take a picture, otherwise,
-            // just pick from photo library
-            
-            
-            /* if UIImagePickerController.isSourceTypeAvailable(.camera) {
-             imagePicker.sourceType = .camera
-             
-             } else {*/
-            imagePicker.sourceType = .photoLibrary
-            //}
-            
-            imagePicker.delegate = self
-            
-            // Place image picker on the screen
-            present(imagePicker, animated: true, completion: nil)
-        }
-        
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
-            let image1 = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-            
-            // Store the image in the ImageStore for the item's key
-            //imageStore.setImage(image1, forKey: item.itemKey)
-            
-            // Put that image onto the screen in our image view
-            
-            image.image = image1
-            
-            // Take image picker off the screen -
-            // you must call this dismiss method
-            dismiss(animated: true, completion: nil)
-        }
-    
-
-        
-    
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.tabBar.isHidden = true
     }
